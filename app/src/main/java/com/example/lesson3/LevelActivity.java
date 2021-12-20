e564eaa3f02e562c2a448dbd04e7cd05efce22b7
muradst <krasnodarceboksary@gmail.com>
2021-12-20 13:34:53 +0600

    тотльллтл

--- app/src/main/java/com/example/lesson3/LevelActivity.java
+++ app/src/main/java/com/example/lesson3/LevelActivity.java
@@ -2,13 +2,35 @@ package com.example.lesson3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LevelActivity extends AppCompatActivity {
+    Button btn121;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level);
+        initialization();
+        listeners();
     }
+
+    private void listeners() {
+        btn121.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LevelActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initialization() {
+        btn121 = findViewById(R.id.btn_121);
+    }
+
+
 }
--- app/src/main/java/com/example/lesson3/MainActivity.java
+++ app/src/main/java/com/example/lesson3/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button btnStart;
+    Button btnLevel1;
 
 
     @Override
@@ -17,11 +18,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initialization();
         listeners();
+        levelis();
 
+    }
+
+    private void levelis() {
+        btnLevel1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LevelActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void initialization() {
         btnStart = findViewById(R.id.btn_Start);
+        btnLevel1 = findViewById(R.id.btn_level1);
 
     }
 
